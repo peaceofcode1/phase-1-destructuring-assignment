fa72497d665a38b4b4eedd3dab5db351db48c137
jafar bino <binocoder@gmail.com>
2023-11-18 22:18:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -25,24 +25,29 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
-// Strings
 
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, dolly, babe, little] = farmAnimals.split(' ').filter(animal => animal !== 'horse');
+const [blackAndWhite, black, pink] = farmAnimals.split(' ').filter(animal => animal !== 'horse' && animal !== 'chicken');
 
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, indg, v] = colors;
 
-// Arrays
 
-// 4. Use destructuring to assign appropriate variables using the color names.
+const {
+  muppetName, color, song, job, partner,
+  nestedName, nestedColor, nestedJob, nestedPartner,
+  album: {
+    theMuppetMovie: { song2, song4 }
+  }
+} = { ...muppet, ...nestedMuppet }; 
 
-// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
-
-// Objects
-
-// 7. Use destructuring to assign all variables using the keys as the variable names
-
-// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+console.log(moo, neigh, baa, oink, cluck);
+console.log(bessie, dolly, babe, little);
+console.log(blackAndWhite, black, pink);
+console.log(red, orange, yellow, green, blue, indigo, violet);
+console.log(r, o, y, g, b, indg, v);
+console.log(muppetName, color, song, job, partner);
+console.log(nestedName, nestedColor, nestedJob, nestedPartner, song2, song4);
